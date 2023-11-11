411bd99ee1a8cd4f1a4f67f9be06bf7cdca94a56
Claudia Turro Pujol <cturropujol@gmail.com>
2023-11-11 19:30:08 +0100

    desarrollo de mi componente ListaPosts: inyectado servicio de posts para poder pintar todo el listado y creado filtro para poder filtrarlos por categorias. Tambien creado en el service funcion: filtrarCategoria. De este modo mi filtro de categorias en el html se actualizara con las categorias que se añadan con los nuevos posts.

--- src/app/components/lista-posts/lista-posts.component.ts
+++ src/app/components/lista-posts/lista-posts.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Post } from 'src/app/interfaces/post';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'lista-posts',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista-posts.component.css']
 })
 export class ListaPostsComponent {
+
+  
+  arrPosts: Post[] = [];
+  arrCategorias: string[] = [];
+
+  postsService = inject(PostsService);
+
+  ngOnInit(){
+    this.arrPosts = this.postsService.getAll();
+    this.arrCategorias = this.postsService.getCategoria();
+  }
+
+  filtrarContinente($event:any){
+    if($event.target.value === 'filtrar por categoria'){
+      this.arrPosts = this.postsService.getAll();
+    } else {
+      this.arrPosts = this.postsService.getByCategoria($event.target.value);
+    }
+    
+    
+   
+  }
 
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -14,7 +14,7 @@ export class PostsService {
     titulo: 'Descubriendo los encantos de París',
     texto: 'París, la Ciudad de la Luz, es conocida por su arquitectura icónica, su exquisita gastronomía y su rica historia. En este viaje, exploraremos los encantos de la Torre Eiffel, pasearemos por los Campos Elíseos y nos perderemos en los encantadores barrios de Montmartre. ¡Acompáñanos en esta aventura parisina!',
     autor: 'Viajero Aventurero',
-    imagen: 'https://unsplash.com/es/fotos/torre-eiffel-paris-NT1mJPgni6A',
+    imagen: 'https://images.unsplash.com/photo-1492136344046-866c85e0bf04?q=80&w=2928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     fecha: '2023-01-01',
     categoria: 'Europa',
   },
@@ -22,7 +22,7 @@ export class PostsService {
     titulo: 'Explorando los templos de Kyoto',
     texto: 'Kyoto, en el corazón de Japón, alberga una impresionante colección de templos antiguos y jardines zen. En este viaje, nos sumergiremos en la espiritualidad de Kyoto, visitando templos emblemáticos como el Templo Dorado y el Santuario Fushimi Inari. Descubre la serenidad y la belleza de la cultura japonesa.',
     autor: 'Aventurero Zen',
-    imagen: 'https://unsplash.com/es/fotos/pagoda-rodeada-de-arboles-E_eWwM29wfU',
+    imagen: 'https://images.unsplash.com/photo-1545569341-9eb8b30979d9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     fecha: '2023-02-15',
     categoria: 'Asia',
   },
@@ -30,7 +30,7 @@ export class PostsService {
     titulo: 'Explorando los parajes naturales de Patagonia',
     texto: 'La Patagonia, con sus vastos paisajes y glaciares imponentes, es un paraíso para los amantes de la naturaleza. En esta travesía, recorreremos los majestuosos paisajes de la Patagonia, explorando el Parque Nacional Torres del Paine y maravillándonos con la belleza única de esta región remota.',
     autor: 'Aventurero Naturalista',
-    imagen: 'https://unsplash.com/es/fotos/glaciar-en-el-parque-nacional-los-glaciares-provincia-de-santa-cruz-patagonia-argentina-5RFMPufTKPA',
+    imagen: 'https://images.unsplash.com/photo-1552751753-0fc84ae5b6c8?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     fecha: '2023-03-30',
     categoria: 'America',
   },
@@ -38,7 +38,7 @@ export class PostsService {
     titulo: 'Explorando la magia de Marrakech',
     texto: 'Embárcate en un viaje a Marrakech, la joya de Marruecos. Sumérgete en la fascinante mezcla de la historia y la modernidad mientras paseas por los estrechos callejones de la Medina, exploras los exquisitos jardines de Majorelle y te maravillas con la arquitectura única de la mezquita de Koutoubia. Descubre la magia de los zocos, la rica cultura y la hospitalidad marroquí en cada rincón de esta ciudad encantadora.',
     autor: 'Aventurero del Magreb',
-    imagen: 'https://unsplash.com/es/fotos/textiles-de-varios-colores-colgados-junto-a-edificios-de-hormigon-LhVJaRPweJc', 
+    imagen: 'https://images.unsplash.com/photo-1536237717235-0acadb345d8c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
     fecha: '2023-05-20',
     categoria: 'Africa',
 },
@@ -54,7 +54,7 @@ export class PostsService {
     titulo: 'Explorando la Gran Manzana: Nueva York',
     texto: 'Embárcate en una emocionante aventura en la ciudad que nunca duerme: Nueva York. Descubre la majestuosidad de los rascacielos en Manhattan, pasea por el Central Park, explora la diversidad cultural en los barrios de Brooklyn y disfruta de la energía única de Times Square. Prepárate para sumergirte en la mezcla de historia, arte y vida urbana que hace de Nueva York una experiencia inolvidable.',
     autor: 'Aventurero Urbano',
-    imagen: 'https://unsplash.com/es/fotos/autos-estacionados-cerca-del-edificio-marron-wOj5odhDOZ0',
+    imagen: 'https://images.unsplash.com/photo-1492666673288-3c4b4576ad9a?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     fecha: '2023-07-25',
     categoria: 'America',
   },
@@ -62,7 +62,7 @@ export class PostsService {
     titulo: 'Aventuras entre los templos de Angkor Wat',
     texto: 'Camboya es el hogar de los majestuosos templos de Angkor Wat, una maravilla arquitectónica que te transportará a la grandeza del Imperio Jemer. Explora los intrincados pasillos, admira los relieves tallados y maravíllate con la serenidad de este sitio declarado Patrimonio de la Humanidad.',
     autor: 'Aventurero Cultural',
-    imagen: 'https://unsplash.com/es/fotos/angkor-wat-camboya-EY3tC81nFt0',
+    imagen: 'https://images.unsplash.com/photo-1566706546199-a93ba33ce9f7?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     fecha: '2023-08-15',
     categoria: 'Asia',
   },
@@ -70,14 +70,14 @@ export class PostsService {
     titulo: 'Safari en el Delta del Okavango',
     texto: 'Sumérgete en la belleza única del Delta del Okavango en Botswana, donde la vida silvestre prospera en uno de los entornos más impresionantes de África. Experimenta un safari acuático, avistando elefantes, hipopótamos y aves exóticas en este paraíso natural. ¡Descubre la magia del Okavango!',
     autor: 'Aventurero Delta',
-    imagen: 'https://unsplash.com/es/fotos/elefante-gris-en-el-cuerpo-de-agua-durante-el-dia-AMk9rGzVSHs',
+    imagen: 'https://images.unsplash.com/photo-1620002742217-7bf1a4b79c76?q=80&w=2831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     fecha: '2023-09-30',
     categoria: 'Africa',
   },
     ]
    }
 
-  getAll(){
+  getAll(): Post[]{
     return this.postsArr;
   }
 
@@ -85,8 +85,12 @@ export class PostsService {
     this.postsArr.push(post);
   }
 
-  getByCategoria(categoria: string){
-    return this.postsArr.filter((post) => {post.categoria === categoria})
+  getCategoria(): string[]{
+    return [...new Set(this.postsArr.map(post => post.categoria))];
+  }
+
+  getByCategoria(categoria: string): Post[]{
+    return this.postsArr.filter(post => post.categoria === categoria)
 
   }
 }
