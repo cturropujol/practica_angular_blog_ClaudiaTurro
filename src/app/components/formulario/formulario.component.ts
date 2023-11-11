dc87a5e4beac0e4b877cfb03c3df5ef24b555e54
Claudia Turro Pujol <cturropujol@gmail.com>
2023-11-11 20:17:29 +0100

    creado estructura basica formulario y aplicado metodo create en funcion onSubmit

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'formulario',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
 
+  newPost: FormGroup;
+  postsService = inject(PostsService);
+  router = inject(Router)
+
+  constructor(){
+    this.newPost = new FormGroup ({
+      titulo: new FormControl(),
+      texto: new FormControl(),
+      autor: new FormControl(),
+      imagen: new FormControl(),
+      fecha: new FormControl(),
+      categoria: new FormControl(),
+    })
+  }
+
+  onSubmit(){
+    this.postsService.createPost(this.newPost.value);
+    this.router.navigate(['/posts'])
+  }
 }
